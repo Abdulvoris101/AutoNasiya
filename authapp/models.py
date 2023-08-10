da1197f0219623857cc00ae7679ce7284335368f
Abdulvoris101 <erkinovabdulvoris101@gmail.com>
2023-08-10 19:26:05 +0500

    Created Customer and CustomerPurchase models

--- authapp/models.py
+++ authapp/models.py
@@ -1,3 +1,26 @@
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from .managers import CustomUserManager
+import shortuuid
 
 
+class Customer(AbstractBaseUser, PermissionsMixin):
+    checkId = models.CharField("Check Id", max_length=25, default=shortuuid.uuid)
+    firstName = models.CharField("Ism", max_length=255)
+    phoneNumber = models.CharField("Tel.raqam", max_length=255)
+    
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_superuser = models.BooleanField(default=False)
+    
+    createdAt = models.DateTimeField(auto_now_add=True)
+    updatedAt = models.DateTimeField(auto_now=True)
+
+    USERNAME_FIELD = "checkId"
+    REQUIRED_FIELDS = ["firstName", "phoneNumber"]
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return f"{self.firstName}-{self.phoneNumber}"
--- core/models.py
+++ core/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from authapp.models import Customer
+from django.utils import timezone
 
-# Create your models here.
+
+class CustomerPurchase(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    productName = models.CharField("Maxsulot nomi", max_length=255)
+    costProduct = models.FloatField("Maxsulot tan narxi")
+    startingFee = models.FloatField("Boshlang'ich to'lov")
+    taxRate = models.FloatField("Soliq foizi")
+    paymentDay = models.IntegerField("To'lov kuni", default=15)
+    monthlyPaymentAmount = models.FloatField("Oylik tolov", null=True, blank=True)
+    totalPrice = models.FloatField("Ummumiy summa", null=True, blank=True)
+
+    duration = models.IntegerField(null=True, blank=True)
+    startedAt = models.DateTimeField("Boshlanish sanasi", default=timezone.now)
+    finishedAt = models.DateTimeField("Tugash sanasi")
+
+    def __str__(self):
+        return f"{self.customer.checkId} - {self.productName}"
