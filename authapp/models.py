7be4fadf4ed9cbc13d0ebbac1326f5bd34d11bc2
Abdulvoris101 <erkinovabdulvoris101@gmail.com>
2023-08-10 19:50:02 +0500

    CHANGE: changed customer model and deleted abstract user inherit

--- authapp/models.py
+++ authapp/models.py
@@ -1,14 +1,11 @@
-from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
-from django.utils.translation import gettext_lazy as _
-from .managers import CustomUserManager
 import shortuuid
 
 
-class Customer(AbstractBaseUser, PermissionsMixin):
+class Customer(models.Model):
     checkId = models.CharField("Check Id", max_length=25, default=shortuuid.uuid)
     firstName = models.CharField("Ism", max_length=255)
-    phoneNumber = models.CharField("Tel.raqam", max_length=255)
+    phoneNumber = models.CharField("Tel.raqam", max_length=255, unique=True)
     
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
@@ -16,11 +13,6 @@ class Customer(AbstractBaseUser, PermissionsMixin):
     
     createdAt = models.DateTimeField(auto_now_add=True)
     updatedAt = models.DateTimeField(auto_now=True)
-
-    USERNAME_FIELD = "checkId"
-    REQUIRED_FIELDS = ["firstName", "phoneNumber"]
-
-    objects = CustomUserManager()
 
     def __str__(self):
         return f"{self.firstName}-{self.phoneNumber}"
--- autonasiya/settings.py
+++ autonasiya/settings.py
@@ -30,6 +30,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "core",
+    "authapp"
 ]
 
 MIDDLEWARE = [
@@ -95,7 +97,6 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
