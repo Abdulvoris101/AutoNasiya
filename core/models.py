ce480dcd421f1914ab60c830085650c9bcd480cd
Abdulvoris101 <erkinovabdulvoris101@gmail.com>
2023-08-11 16:43:22 +0500

    ADD: added paymend and financial status models

--- core/models.py
+++ core/models.py
@@ -1,1 +1,15 @@
 from django.db import models
+from authapp.models import Customer
+from django.utils import timezone
+
+class Payment(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name="payments")
+    date = models.DateField("To'lov Sanasi", default=timezone.now)
+    amount = models.FloatField("Summa")
+    createdAt = models.DateTimeField(auto_now_add=True)
+
+class FinancialStatus(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name="financies")
+    amount = models.FloatField("Summa")
+
+    createdAt = models.DateTimeField(auto_now_add=True)
