078e98c76f8693a5feb5dbe9fbcf8c971e7b5812
Abdulvoris101 <erkinovabdulvoris101@gmail.com>
2023-08-11 16:27:40 +0500

    Added admins for customer and customer purchase

--- authapp/admin.py
+++ authapp/admin.py
@@ -1,5 +1,21 @@
 from django.contrib import admin
-from .models import Customer
+from .models import Customer, CustomerPurchase
 
-admin.site.register(Customer)
 
+class CustomerPurchaseInline(admin.TabularInline):
+    model = CustomerPurchase
+    readonly_fields = ("id", "totalPrice", "amountOfMonth", "duration")
+    extra = 1
+
+
+@admin.register(CustomerPurchase)
+class CustomerAdmin(admin.ModelAdmin):
+    readonly_fields = ("id", "totalPrice", "amountOfMonth", "duration")
+    list_display = ("customer", "productName", "amountOfMonth", "totalPrice", "duration")
+
+
+@admin.register(Customer)
+class Customer(admin.ModelAdmin):
+    inlines = [CustomerPurchaseInline]
+    list_display = ("firstName", "phoneNumber", "checkId")
+    search_fields = ("phoneNumber", "checkId", "firstName")
--- authapp/models.py
+++ authapp/models.py
@@ -1,18 +1,44 @@
 from django.db import models
 import shortuuid
+from django.utils import timezone
+
 
 
 class Customer(models.Model):
     checkId = models.CharField("Check Id", max_length=25, default=shortuuid.uuid)
     firstName = models.CharField("Ism", max_length=255)
     phoneNumber = models.CharField("Tel.raqam", max_length=255, unique=True)
-    
-    is_staff = models.BooleanField(default=False)
-    is_active = models.BooleanField(default=True)
-    is_superuser = models.BooleanField(default=False)
-    
     createdAt = models.DateTimeField(auto_now_add=True)
     updatedAt = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f"{self.firstName}-{self.phoneNumber}"
+        return f"{self.phoneNumber} - {self.purchases.first().productName}"
+
+
+    class Meta:
+        verbose_name = "Mijoz"
+        verbose_name_plural = "Mijozlar"
+
+
+class CustomerPurchase(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name="purchases")
+    productName = models.CharField("Maxsulot nomi", max_length=255)
+    costProduct = models.FloatField("Maxsulot tan narxi")
+    startingFee = models.FloatField("Boshlang'ich to'lov", default=0.0)
+    taxRate = models.FloatField("Soliq foizi")
+    paymentDay = models.IntegerField("To'lov kuni", default=15)
+    amountOfMonth = models.FloatField("Oylik tolov", null=True, blank=True)
+    totalPrice = models.FloatField("Ummumiy summa", null=True, blank=True)
+
+    duration = models.IntegerField(null=True, blank=True)
+    startedAt = models.DateTimeField("Boshlanish sanasi", default=timezone.now)
+    finishedAt = models.DateTimeField("Tugash sanasi")
+
+
+    class Meta:
+        verbose_name = "Maxsulot"
+        verbose_name_plural = "Maxsulotlar"
+
+
+    def __str__(self):
+        return f"{self.customer.checkId}, {self.productName}"
